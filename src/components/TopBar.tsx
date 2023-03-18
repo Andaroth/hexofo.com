b8901b4e537f69fb9e0965dbdde85c962c881919
Axel <ax.fiolle@gmail.com>
2023-03-18 13:26:49 +0100

    improve topbar display

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,9 +5,11 @@ import { FlexProps, Stack, Flex, Spacer, Text, Image } from '@chakra-ui/react';
 
 const TopBar: FC<FlexProps> = (props) => {
   const [visible,setVisible] = useState(false);
+  const [logoVisible,setLogoVisible] = useState(false);
 
   window.addEventListener('scroll', () => {
-    setVisible(window.scrollY > (window.innerHeight - 48))
+    setLogoVisible(window.scrollY > (window.innerHeight - 48))
+    setVisible(window.scrollY > (window.innerHeight / 2))
   })
 
   return <Flex
@@ -33,7 +35,7 @@ const TopBar: FC<FlexProps> = (props) => {
   >
     <Flex w="100%">
       <Scroll to="top" smooth={true}>
-        <Image cursor="pointer" src="./logo512.png" alt="logo" w="48px" h="48px" transition="all .5s ease" transform={ visible ? 'none' : 'translateY(-48px)'} />
+        <Image cursor="pointer" src="./logo512.png" alt="logo" w="48px" h="48px" transition="all .5s ease" transform={ logoVisible ? 'none' : 'translateY(-48px)'} />
       </Scroll>
       <Spacer />
       <Stack cursor="pointer"  ml="4"_hover={{borderBottom: "2px solid #0F0", color: "#0F0"}}>
@@ -43,7 +45,7 @@ const TopBar: FC<FlexProps> = (props) => {
       </Stack>
       <Stack cursor="pointer" ml="4" _hover={{borderBottom: "2px solid #0F0", color: "#0F0"}}>
         <Scroll to="upcoming" smooth={true}>
-          <Text lineHeight="48px"><span className="noMobile">Nos&nbsp;</span>Soirées</Text>
+          <Text lineHeight="48px"><span className="noMobile">Nos&nbsp;</span>Fêtes</Text>
         </Scroll>
       </Stack>
       <Stack cursor="pointer" ml="4" _hover={{borderBottom: "2px solid #0F0", color: "#0F0"}}>
