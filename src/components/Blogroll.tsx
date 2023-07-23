836ec55cd4ccd65f53848dc74fbc2182b63c7b5f
Axel <ax.fiolle@gmail.com>
2023-07-23 13:26:38 +0200

    Improve post fetching in blog

--- src/components/Blogroll.tsx
+++ src/components/Blogroll.tsx
@@ -18,7 +18,7 @@ const Blogroll: FC = () => {
     if (!wpArticles.length) {
       fetch(WP_API + "media?per_page=30").then((res) => res.json()).then(medias => {
         setMedias(medias)
-        fetch(WP_API + "posts").then((res) => res.json()).then(res => {
+        fetch(WP_API + "posts?categories=1,3,6").then((res) => res.json()).then(res => {
           const articles: Array<wpArticle> = []
           res.forEach((article: any) => {
             const matchMedia = wpMedias.find((media: any) => media.id === article.featured_media)
@@ -37,22 +37,24 @@ const Blogroll: FC = () => {
   })
 
   return <Stack id="blog" bg="black" background="#070" py="4" style={{ marginTop: '0'}}>
-    <Text as="h3" fontSize="xl" color="white" textAlign="center">Articles récents</Text>
-      <Flex mt="4">
-        <Flex w="100vw" overflowX="auto" justifyContent="left" pl="48px">
-          { wpArticles.length ? wpArticles.map((article: wpArticle) => <Stack key={wpArticles.indexOf(article)}
-            cursor="pointer"
-            onClick={() => window.open(article.url, '_blank')}
-            w="200px !impo"
-            mr="2"
-          >
-            <AspectRatio w="200px" ratio={1}>
-              <Image src={ article.thumbnail } alt={ article.alt } w="200px" h="200px" borderRadius="xl" />    
-            </AspectRatio>
-            <Flex maxW="200px" overflow="hidden">
-              <Text color="white" textOverflow="ellipsis">{ article.title }</Text>
-            </Flex>
-          </Stack>) : ""}
+    <Text as="h3" fontSize="xl" color="white" textAlign="center">Articles de notre blog</Text>
+      <Flex mt="4" justifyContent="center">
+        <Flex maxW="100vw" overflowX="auto" justifyContent="left" pl="48px">
+          <Flex alignSelf="center">
+            { wpArticles.length ? wpArticles.map((article: wpArticle) => <Stack key={wpArticles.indexOf(article)}
+              cursor="pointer"
+              onClick={() => window.open(article.url, '_blank')}
+              w="200px !impo"
+              mr="2"
+            >
+              <AspectRatio w="200px" ratio={1}>
+                <Image src={ article.thumbnail } alt={ article.alt } w="200px" h="200px" borderRadius="xl" />    
+              </AspectRatio>
+              <Flex maxW="200px" overflow="hidden">
+                <Text color="white" textOverflow="ellipsis">{ article.title }</Text>
+              </Flex>
+            </Stack>) : ""}
+          </Flex>
       </Flex>
     </Flex>
   </Stack>
